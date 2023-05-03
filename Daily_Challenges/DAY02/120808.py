5c2eb535f78986e16f13383d3cdc289755633b73
sunpaaaa <sunpaaaa@gmail.com>
2023-05-03 11:37:19 +0900

    DAY02 Python

---
+++ Daily_Challenges/DAY02/120808.py
@@ -0,0 +1,14 @@
+# 분수의 덧셈
+# 문제 설명
+# 첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다. 두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+# 제한사항
+# 0 <numer1, denom1, numer2, denom2 < 1,000
+
+def solution(numer1, denom1, numer2, denom2):
+    numer = numer1 * denom2 + numer2 * denom1
+    denom = denom1 * denom2
+    for i in range(2, min(numer, denom)):
+        while numer % i == 0 and denom % i == 0 :
+            numer , denom = numer // i, denom // i
+    return [numer, denom]
