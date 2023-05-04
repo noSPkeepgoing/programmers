68f296cc9cd5f52e82203b8b4e696434ee6437e9
sunpaaaa <sunpaaaa@gmail.com>
2023-05-04 09:40:38 +0900

    DAY03 Python

---
+++ Daily_Challenges/DAY03/120812.py
@@ -0,0 +1,11 @@
+# 최빈값 구하기
+# 문제 설명
+# 최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다. 
+# 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요. 최빈값이 여러 개면 -1을 return 합니다.
+
+def solution(array):
+    num = list(set(array))
+    cnt = [array.count(i) for i in num]
+    idx = cnt.index(max(cnt))
+    answer = num[idx] if cnt.count(max(cnt)) == 1 else -1
+    return answer
