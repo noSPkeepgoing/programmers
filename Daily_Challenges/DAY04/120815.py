f5c9e3e75d584223b499ccc9c63cf227df661fd0
sunpaaaa <sunpaaaa@gmail.com>
2023-05-05 11:00:10 +0900

    DAY04 Python

---
+++ Daily_Challenges/DAY04/120815.py
@@ -0,0 +1,13 @@
+# 피자 나눠 먹기 (2)
+# 문제 설명
+# 머쓱이네 피자가게는 피자를 여섯 조각으로 잘라 줍니다. 
+# 피자를 나눠먹을 사람의 수 n이 매개변수로 주어질 때, 
+# n명이 주문한 피자를 남기지 않고 모두 같은 수의 피자 조각을 먹어야 한다면 최소 몇 판을 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+
+def gcd(a, b) :
+    return b if a % b == 0 else gcd(b, a % b)
+
+def solution(n):
+    gcd_ = gcd(n, 6)
+    lcm = n * 6 // gcd_
+    return lcm // 6
---
+++ Daily_Challenges/DAY04/120817.py
@@ -0,0 +1,6 @@
+# 배열의 평균값
+# 문제 설명
+# 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+
+def solution(numbers):
+    return sum(numbers) / len(numbers)
