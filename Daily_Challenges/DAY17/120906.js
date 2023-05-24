d6d2518252c1ff43b54016ca2a3a10850158c178
sunpaaaa <sunpaaaa@gmail.com>
2023-05-24 21:19:33 +0900

    DAY17 JavaScript

---
+++ Daily_Challenges/DAY17/120906.js
@@ -0,0 +1,10 @@
+// 자릿수 더하기
+// 문제 설명
+// 정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요
+
+function solution(n) {
+  return n
+    .toString()
+    .split('')
+    .reduce((acc, cur) => acc + ~~cur, 0);
+}
