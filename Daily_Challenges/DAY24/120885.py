7ae08dcf4b784a8087328c75f8740ee2861ab70c
sunpaaaa <sunpaaaa@gmail.com>
2023-06-07 20:08:10 +0900

    DAY24 Python

---
+++ Daily_Challenges/DAY24/120885.py
@@ -0,0 +1,6 @@
+# 이진수 더하기
+# 문제 설명
+# 이진수를 의미하는 두 개의 문자열 bin1과 bin2가 매개변수로 주어질 때, 두 이진수의 합을 return하도록 solution 함수를 완성해주세요.
+
+def solution(bin1, bin2):
+    return "{0:b}".format(int(bin1, 2) + int(bin2, 2))
