25b966a93e5bddc67cecb499890f78bd6607b2d5
sunpaaaa <sunpaaaa@gmail.com>
2023-06-09 11:20:04 +0900

    DAY25 JavaScript

---
+++ Daily_Challenges/DAY25/120924.js
@@ -0,0 +1,12 @@
+// 다음에 올 숫자
+// 문제 설명
+// 등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+
+function solution(common) {
+  [a, b, c] = common;
+  if (b - a === c - b) {
+    return common.pop() + (b - a);
+  } else {
+    return common.pop() * (b / a);
+  }
+}
