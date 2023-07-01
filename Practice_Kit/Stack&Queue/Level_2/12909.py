76c88d8512ceb373f097e1ad46c61ed3535ba841
sunpaaaa <sunpaaaa@gmail.com>
2023-07-01 14:30:26 +0900

    Reset file path

--- Practice_Kit/Stack&Queue/Level_2/12909.py
+++ Practice_Kit/Stack&Queue/Level_2/12909.py
@@ -6,3 +6,20 @@
 # ")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
 # '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고,
 #  올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+def solution(s):
+    stack = []
+
+    for _ in s:
+        if _ == '(':
+            stack.append(_)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                return False
+
+    if len(stack):
+        return False
+    else:
+        return True
